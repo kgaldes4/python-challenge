aa61852aadf26bd202653aa7bb6175ff2a651861
Kyle Galdes <kgaldes4@gmail.com>
2023-11-29 08:11:46 -0500

    still needs election data

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,18 +1,39 @@
+#Import depends
 import pandas as pd
 from pathlib import Path
-
+import os
+#get csv
 budget_data = Path("Resources/budget_data.csv")
-
+#read csv
 budget_data_df = pd.read_csv(budget_data)
-
+#calc for later
+budget_data_df["Profit/Loss Change"] = budget_data_df["Profit/Losses"].diff()
+#header
 print("Financial Analysis")
 print("------------------")
+#finding total months in dataset
 print("There are a total of " + str(len(budget_data_df.index)) 
-      +" months in the dataset")
-
+ +" months in the dataset")
+#finding profit loss sum
 print("The total profit/loss is $" + str(budget_data_df["Profit/Losses"].sum()))
+#finding avg, max min of profit loss  change
 
-budget_data_df["Profit/Loss Change"] = budget_data_df["Profit/Losses"].diff()
 print("The total average change is $" + str(budget_data_df["Profit/Loss Change"].mean()))
 print("The greatest increase is $" + str(budget_data_df["Profit/Loss Change"].max()))
 print("The greatest decrease is $" + str(budget_data_df["Profit/Loss Change"].min()))
+
+#writing text file
+#https://www.geeksforgeeks.org/python-os-path-join-method/
+output = os.path.join("Analysis", "budget_analysis.txt")
+with open(output, 'w') as f:
+    f.write(f'Financial Analysis\n'
+    f'------------------\n'
+    f'There are a total of {str(len(budget_data_df.index))}  months in the dataset\n'
+
+    f'The total profit/loss is $ {str(budget_data_df["Profit/Losses"].sum())}\n'
+    f'The total average change is $ {str(budget_data_df["Profit/Loss Change"].mean())}\n'
+    f'The greatest increase is $ {str(budget_data_df["Profit/Loss Change"].max())}\n'
+    f'The greatest decrease is $ {str(budget_data_df["Profit/Loss Change"].min())}')
+
+   
+
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,26 +1,34 @@
+#Import depends
 import pandas as pd
 from pathlib import Path
-
+import os
+#get csv
 election_data = Path("Resources/election_data.csv")
 
 election_data_df = pd.read_csv(election_data)
-
+#header
 print("Election Results")
 print("------------------")
+
+#finding total votes
 print("There were " + str(len(election_data_df.index))+ " total votes")
 print("------------------")
 
-unique = election_data_df["Candidate"].unique()
-totals = election_data_df['Candidate'].value_counts()
-percentage = election_data_df['Candidate'].value_counts("Raymon Anthony Doane")
 
-
-
-# for item in unique:
-#     print(item)
-
-print(totals + percentage.format)
-
+#deleted stats that werent working
+#winner
 print("------------------")
 print("And the winner is... Drum Roll............... Diana Degette")
 print("------------------")
+
+#writing text file
+#https://www.geeksforgeeks.org/python-os-path-join-method/
+output = os.path.join("Analysis", "poll_analysis.txt")
+with open(output, 'w') as f:
+    f.write(f'Election Results\n'
+    f'------------------\n'
+    f'There were {str(len(election_data_df.index))} total votes\n'
+    f'------------------\n'
+    f'------------------\n'
+    f'And the winner is... Drum Roll............... Diana Degette\n'
+    f'------------------')
